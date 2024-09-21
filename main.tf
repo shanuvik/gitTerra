42037b6821133c3383e533516540133f707dda46
Vikram <vikram.purohits@.com>
2024-09-21 19:26:35 +0530

    my-commit-3-for-subnet-2-create

--- main.tf
+++ main.tf
@@ -11,3 +11,9 @@ resource "aws_subnet" "sub-1" {
 	vpc_id = aws_vpc.myvpc01.id
 	cidr_block = "10.0.1.0/24"
 	}
+
+resource "aws_subnet" "sub-2" {
+        vpc_id = aws_vpc.myvpc01.id
+        cidr_block = "10.0.2.0/24"
+        }
+
